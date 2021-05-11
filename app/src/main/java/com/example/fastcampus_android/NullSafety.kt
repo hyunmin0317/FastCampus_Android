8c66c2ffb9351b9eb7307d39964e948aa075c176
hyunmin0317 <choihm9903@naver.com>
2021-05-11 22:09:43 +0900

    chap05-NullSafety

---
+++ app/src/main/java/com/example/fastcampus_android/NullSafety.kt
@@ -0,0 +1,33 @@
+package com.example.fastcampus_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class NullSafety : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_null_safety)
+
+        val number : Int = 10
+        val number1 : Int? = null
+
+        val number3 = number1?.plus(number)
+        Log.d("number", "number3: " + number3)
+
+        // 삼항연산자 -> 엘비스 연산자(?:)
+        // Null safety 를 위한 도구
+        val number4 = number1 ?: 10
+        Log.d("number", "number4: " + number4)
+
+        fun plus(a: Int, b: Int?): Int {
+            if (b != null) return a+b
+            else return a
+        }
+
+        fun plus2(a: Int, b: Int?): Int? {
+            return b?.plus(a)
+        }
+
+    }
+}
