fb39c2a2c70992d24797c644b936458536c379e0
hyunmin0317 <choihm9903@naver.com>
2021-05-11 12:24:13 +0900

    chap05-activity

--- app/src/main/java/com/example/fastcampus_android/MainActivity.kt
+++ app/src/main/java/com/example/fastcampus_android/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.fastcampus_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.scrollview)
+
+        Log.d("life_cylce", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life_cylce", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life_cylce", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life_cylce", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life_cylce", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life_cylce", "onDestroy")
     }
 }
