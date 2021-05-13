f1b3427b0cc52827ff75abbc4cc3ede9a9fd5787
hyunmin0317 <choihm9903@naver.com>
2021-05-13 16:39:29 +0900

    Update chap05

--- app/src/main/java/com/example/fastcampus_android/ListViewActivity.kt
+++ app/src/main/java/com/example/fastcampus_android/ListViewActivity.kt
@@ -2,12 +2,15 @@ package com.example.fastcampus_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_list_view.*
+import org.w3c.dom.Text
 
 class ListViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +18,19 @@ class ListViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_list_view)
 
         val carList = ArrayList<CarForList>()
-        for (i in 0 until 10) {
+        for (i in 0 until 50) {
             carList.add(CarForList(""+i+"번째 자동차", ""+i+"순위 엔진"))
         }
 
         val adapter = ListViewAdapter(carList, LayoutInflater.from(this@ListViewActivity))
         listView.adapter = adapter
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val carName = (adapter.getItem(position) as CarForList).name
+            val carEngine = (adapter.getItem(position) as CarForList).engine
+
+            Toast.makeText(this@ListViewActivity, carName+""+carEngine, Toast.LENGTH_SHORT).show()
+        }
+        
     }
 }
 
@@ -45,13 +55,40 @@ class ListViewAdapter(
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = layoutInflater.inflate(R.layout.item_view, null)
-        val carNameTextView = view.findViewById<TextView>(R.id.car_name)
-        val carEngineTextView = view.findViewById<TextView>(R.id.car_engine)
+        val view : View
+        val holder : ViewHolder
 
-        carNameTextView.setText(carForList.get(position).name)
-        carEngineTextView.setText(carForList.get(position).engine)
+        if (convertView == null) {
+            Log.d("convert", "1")
+            view = layoutInflater.inflate(R.layout.item_view, null)
+            holder = ViewHolder()
+
+            holder.carName = view.findViewById(R.id.car_name)
+            holder.carEngine = view.findViewById(R.id.car_engine)
+
+            view.tag = holder
+        } else {
+            Log.d("convert", "2")
+            holder = convertView.tag as ViewHolder
+            view = convertView
+        }
+        holder.carName?.setText(carForList.get(position).name)
+        holder.carEngine?.setText(carForList.get(position).engine)
 
         return view
+
+//        val view = layoutInflater.inflate(R.layout.item_view, null)
+//        val carNameTextView = view.findViewById<TextView>(R.id.car_name)
+//        val carEngineTextView = view.findViewById<TextView>(R.id.car_engine)
+//
+//        carNameTextView.setText(carForList.get(position).name)
+//        carEngineTextView.setText(carForList.get(position).engine)
+//
+//        return view
     }
+}
+
+class ViewHolder {
+    var carName: TextView? = null
+    var carEngine: TextView? = null
 }
