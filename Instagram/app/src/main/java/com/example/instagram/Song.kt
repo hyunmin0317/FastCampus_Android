59da5d67bc945c7f49d190514fb437b44adbc8e4
hyunmin0317 <choihm9903@naver.com>
2021-08-13 16:44:05 +0900

    Update Instagram

---
+++ Instagram/app/src/main/java/com/example/instagram/Song.kt
@@ -0,0 +1,9 @@
+package com.example.instagram
+
+import java.io.Serializable
+
+class Song(
+    val title : String? = null,
+    val thumbnail : String? = null,
+    val song : String? = null
+):Serializable
