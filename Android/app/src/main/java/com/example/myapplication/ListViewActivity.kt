5926575ecabd8d961d3b9964427e3bd9b37a548a
hyunmin0317 <choihm9903@naver.com>
2021-08-16 14:16:30 +0900

    Android-ListView

---
+++ Android/app/src/main/java/com/example/myapplication/ListViewActivity.kt
@@ -0,0 +1,68 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_list_view.*
+
+class ListViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+
+        val carList = ArrayList<CarForList>()
+        for (i in 0 until 10) {
+            carList.add(CarForList(""+i+"번째 자동차", ""+i+"순위 엔진"))
+        }
+
+        val adapter = ListViewAdapter(carList, this@ListViewActivity)
+        listView.adapter = adapter
+
+        // Listener 달기
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val carName = (adapter.getItem(position) as CarForList).name
+            val carEngine = (adapter.getItem(position) as CarForList).engine
+
+            Toast.makeText(this@ListViewActivity, carName+" "+carEngine, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+class ListViewAdapter(
+    val carForList: ArrayList<CarForList>,
+    val context: Context
+) : BaseAdapter() {
+
+    // 그리고자 하는 아이템 리스트의 전체 갯수
+    override fun getCount(): Int {
+        return carForList.size
+    }
+
+    // 그리고자 하는 아이템 리스트의 하나(포지션에 해당하는)
+   override fun getItem(position: Int): Any {
+        return carForList.get(position)
+    }
+
+    // 해당 포지션에 위치해 있는 아이템뷰의 아이디 설정
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val layoutInflater = LayoutInflater.from(context)
+        val view = layoutInflater.inflate(R.layout.item_view, null)
+
+        val carNameTextview = view.findViewById<TextView>(R.id.car_name)
+        val carEngineTextview = view.findViewById<TextView>(R.id.car_engine)
+
+        carNameTextview.setText(carForList.get(position).name)
+        carEngineTextview.setText(carForList.get(position).engine)
+        return view
+    }
+}
