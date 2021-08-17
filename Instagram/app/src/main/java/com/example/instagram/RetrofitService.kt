f3bfa15ad8d6b31993f009d49887adeea45c4b3c
hyunmin0317 <choihm9903@naver.com>
2021-08-17 22:53:23 +0900

    Update Instagram-09

--- Instagram/app/src/main/java/com/example/instagram/RetrofitService.kt
+++ Instagram/app/src/main/java/com/example/instagram/RetrofitService.kt
@@ -1,5 +1,7 @@
 package com.example.instagram
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -8,9 +10,9 @@ interface RetrofitService {
     @POST("user/signup/")
     @FormUrlEncoded
     fun register(
-        @Field("username") username : String,
-        @Field("password1") password1 : String,
-        @Field("password2") password2 : String
+        @Field("username") username: String,
+        @Field("password1") password1: String,
+        @Field("password2") password2: String
     ): Call<User>
 
     @POST("user/login/")
@@ -20,7 +22,13 @@ interface RetrofitService {
         @Field("password") password: String
     ): Call<User>
 
-    @GET("instagram/post/list/all")
+    @GET("instagram/post/list/all/")
     fun getAllPosts(): Call<ArrayList<Post>>
 
+    @Multipart
+    @POST("instagram/post/")
+    fun uploadPost(
+        @Part image : MultipartBody.Part,
+        @Part ("content")requestBody : RequestBody
+    ):Call<Post>
 }
